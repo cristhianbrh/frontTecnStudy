482e99e1d0e2f3c785e13ebbb1d4b092044220e9
Cristhian BR <cristhianalexand@gmail.com>
2023-02-06 16:37:45 -0500

    Created and functional options

---
+++ src/config/routing/pathRouting.js
@@ -0,0 +1,10 @@
+import HomePage from "../../components/pages/Home/HomePage";
+
+const routes = [];
+
+routes.push({
+    path: "/Home",
+    view: <HomePage/>
+});
+
+export default routes;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Route, BrowserRouter, Routes, Router } from 'react-router-dom';
+import pathRoute from './config/routing/pathRouting.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-  </>
+  <BrowserRouter>
+    <Routes>
+      {pathRoute.map((routep, index) => 
+        <Route key={index + routep.path} path={routep.path} element={routep.view}/>
+      )}
+    </Routes>
+
+  </BrowserRouter>
 );

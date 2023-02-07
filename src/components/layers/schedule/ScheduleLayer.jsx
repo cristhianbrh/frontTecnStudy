4a2c5f159648b7c2510652f450e848a687345fb8
Cristhian BR <cristhianalexand@gmail.com>
2023-02-06 20:35:38 -0500

    Schedule v1 prototype

---
+++ src/components/layers/schedule/ScheduleLayer.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./style.css";
+const ScheduleLayer = ()=>{
+    const [isVoid, setIsVoid] = useState(true);
+
+    return (
+        <div className="SheduleLayer">
+            <div className="SheduleLayer_init" style={{display: !isVoid?'none':'flex'}}>
+                <div>
+                    <div className="sheduleDay">
+                        <p>Lunes</p>
+                        <div className="active">
+                            <p className="active_Title">Base de datos</p>
+                            <p className="active_Time">7:00am - 10:00am</p>
+                        </div>
+                        <div className="dead">
+                            <p>7:00am - 10:00am</p>
+                        </div>
+                        <div className="active">
+                            <p className="active_Title">Base de datos</p>
+                            <p className="active_Time">7:00am - 10:00am</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="SheduleLayer_isVoid" style={{display: isVoid?'none':'flex'}}>
+                dsds
+            </div>
+        </div>
+    )
+}
+export default ScheduleLayer;
